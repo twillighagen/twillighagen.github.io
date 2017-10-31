3db48d36341dd7cd994e6c17f6e0616488dffbb3
twillighagen <32100447+twillighagen@users.noreply.github.com>
2017-10-31 16:44:56 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Digital Cars</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <?php
+    include_once ('nav.php');
+    ?>
+    </nav>
+  </body>
+</html>
